d8321b8b3e2e19ccf5ec18481fa84e35046be5ea
Luka Kordić <lkordic@hotmail.com>
2017-07-15 11:25:33 +0200

    Implemented onClick Listeners on list items, and created car_details.xml layout

--- app/src/main/java/com/example/cobeosijek/myapplication/Tab2Fragment.java
+++ app/src/main/java/com/example/cobeosijek/myapplication/Tab2Fragment.java
@@ -1,23 +1,34 @@
 package com.example.cobeosijek.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 
 public class Tab2Fragment extends Fragment {
+    public static final String CAR_KEY = "car";
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.tab2_fragment, container, false);
         ListView carListView = view.findViewById(R.id.lv_popular_cars);
+
+        carListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(adapterView.getContext(), CarDetails.class);
+                startActivity(intent);
+            }
+        });
 
         CarAdapter carAdapter = new CarAdapter(this.loadCars());
         carListView.setAdapter(carAdapter);
